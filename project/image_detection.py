89a90cd1ad0d2b1834cb02e2042174888a34c968
Nitro <admin@nitrostudio.dev>
2021-12-08 16:44:27 -0800

    Initialize project files.

---
+++ project/image_detection.py
@@ -0,0 +1,14 @@
+# image_detection.py
+#
+# Copyright 2020 Nitro. All Rights Reserved.
+#
+# Developer: Nitro (admin@nitrostudio.dev)
+# Blog: https://blog.nitrostudio.dev
+# Discord: Nitro#1781
+# GitHub: https://github.com/Nitro1231/MineFish
+#
+# Version: 4.0.0
+# Last Modified: Monday, December 6, 2021 at 11:11 AM. (PDT)
+#
+# This project is licensed under the GNU Affero General Public License v3.0;
+# you may not use this file except in compliance with the License.
