ddbdaa7d78bd5295944872de1fe6b363160a6e14
Nitro <admin@nitrostudio.dev>
2021-12-18 22:51:26 +0900

    Update setting tap GUI.

--- project/minefish.py
+++ project/minefish.py
@@ -16,21 +16,33 @@
 # https://wikidocs.net/21849
 
 import os
-import cv2
 import sys
+
+from cv2 import validateDisparity
 import setting
 import image_detection
 import window_manager
 import pygetwindow
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QTabWidget, QVBoxLayout, QPushButton, QGridLayout, QCheckBox
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QWidget,
+    QTabWidget,
+    QVBoxLayout,
+    QPushButton,
+    QGridLayout,
+    QCheckBox,
+    QComboBox,
+    QSlider,
+    QGroupBox
+)
 from qt_material import apply_stylesheet
 
-SETTING_PATH = './setting.json'
-FREQUENCY = 40
-MIN_SCALE = 0.5
-MAX_SCALE = 2.0
+SETTING_PATH = '.\\setting.json'
+IMAGE_PATH = '.\\image'
+LANGUAGE_PATH = '.\\language'
 WIDTH = 600
 HEIGHT = 400
 MATCH = {
@@ -45,14 +57,6 @@ class MineFish(QWidget):
         self.setting = setting.Setting(SETTING_PATH)
         self.setting.verify_setting()
 
-        self.image_detection = image_detection.ImageDetection(
-            target_path=self.setting.setting['image'],
-            accuracy=self.setting.setting['accuracy'],
-            frequency=FREQUENCY,
-            min_scale=MIN_SCALE,
-            max_scale=MAX_SCALE
-        )
-
         self.initialize_ui()
         self.set_preview_active()
         self.load_resources()
@@ -64,7 +68,7 @@ class MineFish(QWidget):
         apply_stylesheet(self, theme='dark_lightgreen.xml')
 
         preview_tab = self.initialize_preview_tab()
-        setting_tab = QWidget()
+        setting_tab = self.initialize_setting_tab()
         about_tab = QWidget()
 
         main_tabs = QTabWidget()
@@ -96,15 +100,14 @@ class MineFish(QWidget):
         self.capture_label = QLabel('Captured Image')
         self.capture_image = QLabel()
 
-        grid_layout = QGridLayout()
-        grid_layout.addWidget(self.capturing_label, 0, 0)
-        grid_layout.addWidget(self.active_toggle, 0, 0)
-        grid_layout.addWidget(self.target_label, 1, 0)
-        grid_layout.addWidget(self.target_image, 2, 0)
-        grid_layout.addWidget(self.capture_label, 3, 0)
-        grid_layout.addWidget(self.capture_image, 4, 0)
-
-        preview_tab.setLayout(grid_layout)
+        box_layout = QVBoxLayout()
+        box_layout.addWidget(self.capturing_label)
+        box_layout.addWidget(self.active_toggle)
+        box_layout.addWidget(self.target_label)
+        box_layout.addWidget(self.target_image)
+        box_layout.addWidget(self.capture_label)
+        box_layout.addWidget(self.capture_image)
+        preview_tab.setLayout(box_layout)
 
         return preview_tab
 
@@ -128,15 +131,180 @@ class MineFish(QWidget):
     def initialize_setting_tab(self):
         setting_tab = QWidget()
 
+        # Target Image
+        target_image_title = QLabel('Target Image')
+        target_image_list = QComboBox()
+        target_image_list.addItems(self.get_files(IMAGE_PATH))
+        target_image_dialog_button = QPushButton('Open File')
+        target_image_folder_button = QPushButton('Open Folder')
+        
+        target_image_box_layout = QGridLayout()
+        target_image_box_layout.addWidget(target_image_title, 0, 0)
+        target_image_box_layout.addWidget(target_image_list, 1, 0, 1, 8)
+        target_image_box_layout.addWidget(target_image_dialog_button, 1, 9, 1, 1)
+        target_image_box_layout.addWidget(target_image_folder_button, 1, 10, 1, 1)
 
-        grid_layout = QGridLayout()
-        grid_layout.addWidget(self.capturing_label, 0, 0)
+        # Display Language
+        language_title = QLabel('Display Language')
+        language_list = QComboBox()
+        language_list.addItems(self.get_files(LANGUAGE_PATH))
 
-        setting_tab.setLayout(grid_layout)
+        # normal_box
+        # Accuracy
+        accuracy_title = QLabel('Accuracy')
+        accuracy_value = QLabel()
+        accuracy_value.setAlignment(Qt.AlignCenter)
+        accuracy_bar = QSlider(Qt.Horizontal)
+        accuracy_bar.setRange(30, 90)
+        accuracy_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=accuracy_value,
+                key='accuracy',
+                scale=100,
+                value_type=float
+            )
+        )
 
+        # Detection Delay
+        detection_title = QLabel('Detection Delay')
+        detection_value = QLabel()
+        detection_value.setAlignment(Qt.AlignCenter)
+        detection_bar = QSlider(Qt.Horizontal)
+        detection_bar.setRange(10, 50)
+        detection_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=detection_value,
+                key='detection_delay',
+                scale=100,
+                value_type=float
+            )
+        )
+
+        # Throwing Delay
+        throwing_title = QLabel('Throwing Delay')
+        throwing_value = QLabel()
+        throwing_value.setAlignment(Qt.AlignCenter)
+        throwing_bar = QSlider(Qt.Horizontal)
+        throwing_bar.setRange(30, 500)
+        throwing_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=throwing_value,
+                key='throwing_delay',
+                scale=100,
+                value_type=float
+            )
+        )
+
+        normal_box = QVBoxLayout()
+        normal_box.addWidget(accuracy_title)
+        normal_box.addWidget(accuracy_bar)
+        normal_box.addWidget(accuracy_value)
+        normal_box.addStretch(1)
+        normal_box.addWidget(detection_title)
+        normal_box.addWidget(detection_bar)
+        normal_box.addWidget(detection_value)
+        normal_box.addStretch(1)
+        normal_box.addWidget(throwing_title)
+        normal_box.addWidget(throwing_bar)
+        normal_box.addWidget(throwing_value)
+
+        # advanced_box
+        # Frequency
+        frequency_title = QLabel('Frequency')
+        frequency_value = QLabel()
+        frequency_value.setAlignment(Qt.AlignCenter)
+        frequency_bar = QSlider(Qt.Horizontal)
+        frequency_bar.setRange(10, 100)
+        frequency_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=frequency_value,
+                key='frequency',
+                scale=1,
+                value_type=int
+            )
+        )
+
+        # Min Scale
+        min_scale_title = QLabel('Min Scale')
+        min_scale_value = QLabel()
+        min_scale_value.setAlignment(Qt.AlignCenter)
+        min_scale_bar = QSlider(Qt.Horizontal)
+        min_scale_bar.setRange(10, 80)
+        min_scale_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=min_scale_value,
+                key='min_scale',
+                scale=100,
+                value_type=float
+            )
+        )
+        
+        # Max Scale
+        max_scale_title = QLabel('Max Scale')
+        max_scale_value = QLabel()
+        max_scale_value.setAlignment(Qt.AlignCenter)
+        max_scale_bar = QSlider(Qt.Horizontal)
+        max_scale_bar.setRange(90, 250)
+        max_scale_bar.valueChanged.connect(
+            self.make_slider_event(
+                value_display=max_scale_value,
+                key='max_scale',
+                scale=100,
+                value_type=float
+            )
+        )
+
+        advanced_box = QVBoxLayout()
+        advanced_box.addWidget(frequency_title)
+        advanced_box.addWidget(frequency_bar)
+        advanced_box.addWidget(frequency_value)
+        advanced_box.addStretch(1)
+        advanced_box.addWidget(min_scale_title)
+        advanced_box.addWidget(min_scale_bar)
+        advanced_box.addWidget(min_scale_value)
+        advanced_box.addStretch(1)
+        advanced_box.addWidget(max_scale_title)
+        advanced_box.addWidget(max_scale_bar)
+        advanced_box.addWidget(max_scale_value)
+
+        slider_grid = QGridLayout()
+        slider_grid.addLayout(normal_box, 0, 0)
+        slider_grid.addLayout(advanced_box, 0, 1)
+
+        box_layout = QVBoxLayout()
+        box_layout.addLayout(target_image_box_layout)
+        box_layout.addStretch(1)
+        box_layout.addWidget(language_title)
+        box_layout.addWidget(language_list)
+        box_layout.addStretch(1)
+        box_layout.addLayout(slider_grid)
+
+        setting_tab.setLayout(box_layout)
+        
         return setting_tab
 
+    def make_slider_event(self, value_display: 'QLabel()', key: str, scale: int, value_type: 'function') -> 'function':
+        def slider_event(value: int) -> None:
+            value_display.setText(str(value_type(value / scale)))
+        return slider_event
+
+    def get_files(self, path: str) -> list():
+        files = list()
+        for item in os.listdir(path):
+            item_path = os.path.join(path, item)
+            if os.path.isfile(item_path):
+                files.append(item_path)
+        return files
+
     def load_resources(self) -> None:
+        self.image_detection = image_detection.ImageDetection(
+            target_path=self.setting.setting['image'],
+            accuracy=self.setting.setting['accuracy'],
+            frequency=self.setting.setting['frequency'],
+            min_scale=self.setting.setting['min_scale'],
+            max_scale=self.setting.setting['max_scale']
+        )
+
         self.target_pixmap = QPixmap(self.setting.setting['image'])
         self.target_image.setPixmap(self.target_pixmap)
 
